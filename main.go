25ae95895d8772d5df3c6206b9e12710aa615a64
Fábio Oliveira <fabio_net1000@hotmail.com>
2017-07-20 20:05:46 -0300

    web application

--- main.go
+++ main.go
@@ -3,8 +3,18 @@ package main
 import (
 	"github.com/astaxie/beego"
 	_ "github.com/fabiosoliveira/webgo/routers"
+	"os"
+	"strconv"
+	"log"
 )
 
 func main() {
+
+	if porta, erro := strconv.Atoi(os.Getenv("PORT")) ; erro == nil {
+		beego.BConfig.Listen.HTTPPort = porta
+	} else {
+		log.Fatalf("Erro ao passar o http port como argumento: %v", erro.Error())
+	}
+
 	beego.Run()
 }
